dd378557c9fce3229c3bc2e3facdd6716631d963
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2024-06-27 22:20:43 -0600

    Se trabaja con la anotación PathVariable para recuperar parametros desde la URL del navegador

--- src/main/java/cursospringboot/controllers/PathVariableController.java
+++ src/main/java/cursospringboot/controllers/PathVariableController.java
@@ -12,4 +12,28 @@ public class PathVariableController {
 
         return "Hola " +nombre;
     }
+
+    @GetMapping("validar-palindromo/{palabra}")
+    public String palindromo(@PathVariable String palabra){
+
+        if(isPalindromo(palabra)){
+            return "La palabra " +palabra+ " es un palindromo";
+        }
+        else{
+            return "La palabra " +palabra+ " NO es un palindromo";
+        }
+    }
+
+    private boolean isPalindromo(String palabra){
+
+        int longitud = palabra.length();
+
+        for(int i = 0; i < longitud / 2; i++){
+
+            if(palabra.charAt(i) != palabra.charAt(longitud - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
