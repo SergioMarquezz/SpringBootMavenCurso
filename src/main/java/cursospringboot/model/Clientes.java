281d753b4d9f7ff7fde3abdc5902aaedd1511b5c
Sergio Alberto Marquez Moreno <marquezsergioalberto1926@gmail.com>
2024-07-07 23:34:21 -0600

    se agregan atributos a la clase pojo y se crear la clase rest controller para clientees

--- src/main/java/cursospringboot/model/Clientes.java
+++ src/main/java/cursospringboot/model/Clientes.java
@@ -1,4 +1,48 @@
 package cursospringboot.model;
 
 public class Clientes {
+
+    private int idCliente;
+    private String nombre;
+    private String usuario;
+    private String contrasenia;
+
+    public Clientes(int idCliente, String nombre, String usuario, String contrasenia) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
