f1cd9c5ac0ca4f67358eb06a4f6085a3f8e380ce
Sergio Alberto Marquez Moreno <marquezsergioalberto1926@gmail.com>
2024-07-08 22:54:33 -0600

    Se agrega lista de clientes para el consumo midiante la anotacion GetMapping

--- src/main/java/cursospringboot/controllers/ClientesRestController.java
+++ src/main/java/cursospringboot/controllers/ClientesRestController.java
@@ -1,7 +1,26 @@
 package cursospringboot.controllers;
 
+import cursospringboot.model.Clientes;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ClientesRestController {
+
+    private List<Clientes> customers = new ArrayList<>(Arrays.asList(
+
+            new Clientes(123,"Sergio Alberto","serch123", "checo123"),
+            new Clientes(321,"Alberto Marquez","beto321", "321Beto"),
+            new Clientes(654,"Mario Hernandez","bros654", "marioB654"),
+            new Clientes(789,"Diana","dianita987", "789Diana")
+    ));
+
+    @GetMapping("clientes")
+    public List<Clientes> getClientes(){
+        return customers;
+    }
 }
